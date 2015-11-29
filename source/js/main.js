f35229f0eb94fe7b6d896729204072d21bb9a974
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-11-29 03:53:30 -0200

    tab system

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,28 @@
-(function () {
-  console.log('main.js');
-})();
+
+$(function () {
+  var $tabs = $('.tabs');
+
+  $tabs.each(function (index, element) {
+    var $element = $(element);
+    var $buttons = $element.find('.tabs-navigation a');
+    var $items = $element.find('.tabs-item');
+
+    $buttons.on('click', function (e) {
+      e.preventDefault();
+      var index = $buttons.index(this);
+      var $this = $(this);
+
+      $this.parent('li').addClass("active");
+
+      $buttons
+        .not($this)
+        .parent('li')
+        .removeClass('active');
+
+      $items
+        .removeClass('active')
+        .eq(index)
+        .addClass('active');
+    });
+  });
+});
